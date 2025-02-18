f3340211dd7c97a6594451008b9858c5b48f70e9
Gabriel Castor <gabrielcastor1991@gmail.com>
2025-02-18 17:11:29 +0000

    Sketch triggers

--- graph_db/models/graph.py
+++ graph_db/models/graph.py
@@ -2,6 +2,7 @@ from graph_db.models.base import Base
 from sqlalchemy import Column, Integer, ForeignKey
 from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import relationship
+from sqlalchemy import DDL
 
 
 class Unit(Base):
@@ -20,3 +21,37 @@ class Document(Base):
     content = Column(JSONB, nullable=False, server_default="{}")
 
     unit = relationship("Unit", back_populates="documents")
+
+
+create_graph_node_function = DDL(
+    "CREATE FUNCTION add_node() RETURNS TRIGGER AS $$ "
+    "BEGIN NEW.data := 'ins'; "
+    "RETURN NEW; "
+    "END; $$ LANGUAGE PLPGSQL"
+)
+
+# update_graph_node_function = DDL("CREATE FUNCTION update_node() RETURNS TRIGGER AS $$ ")
+
+# delete_graph_node_function = DDL("CREATE FUNCTION delete_node() RETURNS TRIGGER AS $$ ")
+
+# create_graph_node_trigger = DDL("CREATE TRIGGER dt_ins AFTER INSERT ON document ")
+
+# update_graph_node_trigger = DDL("CREATE TRIGGER dt_ins AFTER UPDATE ON document ")
+
+# delete_graph_node_trigger = DDL("CREATE TRIGGER dt_ins AFTER DELETE ON document ")
+
+# event.listen(
+#     Document.__table__,
+#     "after_create",
+#     create_graph_node_function.execute_if(dialect="postgresql"),
+# )
+# event.listen(
+#     Document.__table__,
+#     "after_update",
+#     update_graph_node_function.execute_if(dialect="postgresql"),
+# )
+# event.listen(
+#     Document.__table__,
+#     "after_delete",
+#     delete_graph_node_function.execute_if(dialect="postgresql"),
+# )
