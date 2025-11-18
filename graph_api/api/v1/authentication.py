c90a3d117e0ce8d61138b7a4b5b6643344ec7ef9
Gabriel Castor <gabrielcastor1991@gmail.com>
2025-11-18 00:43:22 +0000

    Refactor authorisation endpoints

--- graph_api/api/v1/authentication.py
+++ graph_api/api/v1/authentication.py
@@ -1,6 +1,4 @@
 import jwt
-import os
-from pydantic import BaseModel, ValidationError
 from fastapi import Depends, APIRouter, HTTPException, Security, status
 from fastapi.security import (
     OAuth2PasswordBearer,
@@ -9,28 +7,45 @@ from fastapi.security import (
 )
 from typing import Annotated
 from datetime import datetime, timedelta, timezone
+from pydantic import BaseModel, ValidationError
+from pwdlib import PasswordHash
 from jwt.exceptions import InvalidTokenError
-from passlib.context import CryptContext
 
-# >> openssl rand -hex 32
-SECRET_KEY = os.getenv("SECRET_KEY")
+# openssl rand -hex 32
+SECRET_KEY = "a4d04de77d7d87915b977587f2d56ad5340267f0b61f25138d1cb63b30da4dbe"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
-HASHED_PASSWORD = os.getenv("HASHED_PASSWORD")
+
+
+route = APIRouter()
+
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="v1/auth/token",
+    scopes={"me": "Read information about the current user.", "items": "Read items."},
+)
+password_hash = PasswordHash.recommended()
+
 
 fake_users_db = {
+    "johndoe": {
+        "name": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$argon2id$v=19$m=65536,t=3,p=4$e/7ugisQS7W+PIrAMUljfg$2kP1D/V3eJ0yRvbR+tE8oRbz4defcQUX5Z8OVIfZJog",
+        "disable": False,
+    },
     "alice": {
         "name": "alice",
-        "fill_name": "Alice Chains",
-        "email": "alicechains@someserver.com",
-        "hashed_password": HASHED_PASSWORD,
-        "disabled": True,
+        "full_name": "Alice Chains",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disable": True,
     },
     "bob": {
         "name": "bob",
         "fill_name": "Bob Marloy",
         "email": "threes@littlebirds.com",
-        "hashed_password": HASHED_PASSWORD,
+        "hashed_password": "fakehashedsecret",
         "disabled": False,
     },
 }
@@ -49,42 +64,88 @@ class TokenData(BaseModel):
 class User(BaseModel):
     name: str
     email: str | None = None
+    nodes: list[str] = []
     full_name: str | None = None
     disabled: bool | None = None
 
 
-class UserInDB(User):
+class UserDBRecord(User):
     hashed_password: str
 
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-oauth2_scheme = OAuth2PasswordBearer(
-    tokenUrl="token",
-    scopes={"me": "Read information about the current user.", "items": "Read items."},
-)
-
-route = APIRouter()
+def get_user(db, username: str) -> UserDBRecord | None:
+    if username in db:
+        user_dict = db[username]
+        return UserDBRecord(**user_dict)
 
 
-def verify_password(plain_password: str, hashed_password: str):
-    return pwd_context.verify(plain_password, hashed_password)
+class InvalidCredentials(HTTPException):
+    def __init__(self, headers: dict = {"WWW-Authenticate": "Bearer"}):
+        super().__init__(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid Authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
 
 
-def get_password_hash(password):
-    return pwd_context.hash(password)
+class InsufficientPermissions(HTTPException):
+    def __init__(self, headers: dict = {"WWW-Authenticate": "Bearer"}):
+        super().__init__(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Not enough permissions",
+            headers=headers,
+        )
 
 
-def get_user(db, name: str):
-    if name in db:
-        return UserInDB(**db[name])
+async def get_current_user(
+    security_scopes: SecurityScopes, token: Annotated[str, Depends(oauth2_scheme)]
+) -> User:
+    if security_scopes.scopes:
+        authenticate_value = f'Bearer scope="{security_scopes.scope_str}"'
+    else:
+        authenticate_value = "Bearer"
+    headers = {"WWW-Authenticate": authenticate_value}
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise InvalidCredentials(headers)
+        token_scopes = payload.get("scopes", [])
+        token_data = TokenData(username=username, scopes=token_scopes)
+    except (InvalidTokenError, ValidationError):
+        raise InvalidCredentials(headers)
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise InvalidCredentials(headers)
+    for scope in security_scopes.scopes:
+        if scope not in token_data.scopes:
+            raise InsufficientPermissions(headers)
+    return user
+
+
+async def get_current_active_user(
+    current_user: Annotated[User, Security(get_current_user, scopes=["me"])],
+):
+    if current_user.disabled:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Inactive user"
+        )
+    return current_user
+
+
+def get_password_hash(password: str):
+    return password_hash.hash(password)
+
+
+def verify_password(plain_password: str | bytes, hashed_password: str | bytes):
+    return password_hash.verify(plain_password, hashed_password)
 
 
 def authenticate_user(
-    fake_db, username: str, password: str
-) -> tuple[bool, UserInDB | None]:
+    fake_db: dict, username: str, password: str
+) -> tuple[bool, UserDBRecord | None]:
     user = get_user(fake_db, username)
-    if not user:
+    if user is None:
         return False, None
     if not verify_password(password, user.hashed_password):
         return False, None
@@ -102,53 +163,6 @@ def create_access_token(data: dict, ttl: timedelta | None = None) -> str:
     return encoded_jwt
 
 
-async def get_current_user(
-    security_scopes: SecurityScopes, token: Annotated[str, Depends(oauth2_scheme)]
-):
-    if security_scopes.scopes:
-        authenticate_value = f'Bearer scope="{security_scopes.scope_str}"'
-    else:
-        authenticate_value = "Bearer"
-
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": authenticate_value},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_scopes = payload.get("scopes", [])
-        token_data = TokenData(scopes=token_scopes, username=username)
-    except (InvalidTokenError, ValidationError):
-        raise credentials_exception
-
-    user = get_user(fake_users_db, username=token_data.username)
-    if user is None:
-        raise credentials_exception
-    for scope in security_scopes.scopes:
-        if scope not in token_data.scopes:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Not enough permissions",
-                headers={"WWW-Authenticate": authenticate_value},
-            )
-    return user
-
-
-async def get_current_active_user(
-    current_user: Annotated[User, Security(get_current_user, scopes=["me"])],
-):
-    if current_user.disabled:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Inactive user"
-        )
-    return current_user
-
-
-# TODO: replace by Auth0
 @route.post("/token")
 async def login_for_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
@@ -156,11 +170,8 @@ async def login_for_access_token(
     authenticated, user = authenticate_user(
         fake_users_db, form_data.username, form_data.password
     )
-    if not authenticated:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Incorrect username or password",
-        )
+    if not authenticated or user is None:
+        raise InvalidCredentials()
     access_token_ttl = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
         data={"sub": user.name, "scopes": form_data.scopes}, ttl=access_token_ttl
@@ -175,13 +186,8 @@ async def read_users_me(
     return current_user
 
 
-# @route.get("/users/me/items/")
-# async def read_own_items(
-#     current_user: Annotated[User, Security(get_current_active_user, scopes=["items"])],
-# ):
-#     return [{"item_id": "Foo", "owner": current_user.username}]
-
-
-# @route.get("/status/")
-# async def read_system_status(current_user: Annotated[User, Depends(get_current_user)]):
-#     return {"status": "ok"}
+@route.get("/users/me/items")
+async def read_own_items(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return [{"item_id": "Foo", "owner": current_user.username}]
