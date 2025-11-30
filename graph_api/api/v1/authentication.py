383550c10f9d59198381d13c2fc8c69d89f5882f
Gabriel Castor <gabrielcastor1991@gmail.com>
2025-11-30 22:28:01 +0000

    Integrate authentication with Auth0

--- graph_api/api/v1/authentication.py
+++ graph_api/api/v1/authentication.py
@@ -1,54 +1,18 @@
 import jwt
+import os
 from fastapi import Depends, APIRouter, HTTPException, Security, status
 from fastapi.security import (
-    OAuth2PasswordBearer,
-    OAuth2PasswordRequestForm,
     SecurityScopes,
 )
 from typing import Annotated
-from datetime import datetime, timedelta, timezone
 from pydantic import BaseModel, ValidationError
-from pwdlib import PasswordHash
 from jwt.exceptions import InvalidTokenError
-
-# openssl rand -hex 32
-SECRET_KEY = "a4d04de77d7d87915b977587f2d56ad5340267f0b61f25138d1cb63b30da4dbe"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+import httpx
+from fastapi.security import OAuth2
+from fastapi.openapi.models import OAuthFlows, OAuthFlowImplicit
 
 
 route = APIRouter()
-
-oauth2_scheme = OAuth2PasswordBearer(
-    tokenUrl="v1/auth/token",
-    scopes={"me": "Read information about the current user.", "items": "Read items."},
-)
-password_hash = PasswordHash.recommended()
-
-
-fake_users_db = {
-    "johndoe": {
-        "name": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "$argon2id$v=19$m=65536,t=3,p=4$e/7ugisQS7W+PIrAMUljfg$2kP1D/V3eJ0yRvbR+tE8oRbz4defcQUX5Z8OVIfZJog",
-        "disable": False,
-    },
-    "alice": {
-        "name": "alice",
-        "full_name": "Alice Chains",
-        "email": "alice@example.com",
-        "hashed_password": "fakehashedsecret",
-        "disable": True,
-    },
-    "bob": {
-        "name": "bob",
-        "fill_name": "Bob Marloy",
-        "email": "threes@littlebirds.com",
-        "hashed_password": "fakehashedsecret",
-        "disabled": False,
-    },
-}
 
 
 class Token(BaseModel):
@@ -62,6 +26,7 @@ class TokenData(BaseModel):
 
 
 class User(BaseModel):
+    id: str
     name: str
     email: str | None = None
     nodes: list[str] = []
@@ -69,14 +34,121 @@ class User(BaseModel):
     disabled: bool | None = None
 
 
-class UserDBRecord(User):
-    hashed_password: str
+class IdentityProvider:
+    issuer: str
+    audience: str
+
+    async def get_public_key(self, kid: str) -> str:
+        raise NotImplementedError()
+
+    async def verify_token(self, token: str, headers: dict) -> dict:
+        raise NotImplementedError()
+
+    async def get_user(self, user_id: str) -> User | None:
+        raise NotImplementedError()
+
+    async def get_oauth2_scheme(self, scopes: dict) -> OAuth2:
+        raise NotImplementedError()
 
 
-def get_user(db, username: str) -> UserDBRecord | None:
-    if username in db:
-        user_dict = db[username]
-        return UserDBRecord(**user_dict)
+class Auth0provider(IdentityProvider):
+    def __init__(self):
+        super().__init__()
+        self.grant_type = "client_credentials"
+        self.client_id = os.getenv("AUTH0_CLIENT_ID")
+        self.client_secret = os.getenv("AUTH0_CLIENT_SECRET")
+
+        auth0_domain = os.getenv("AUTH0_DOMAIN")
+        base_url = f"https://{auth0_domain}"
+
+        self.issuer = base_url
+        self.audience = (
+            os.getenv("AUTH0_API_AUDIENCE")
+            or f"https://{os.getenv('AUTH0_DOMAIN')}/api/v2/"
+            or f"https://whollygrid.com"
+        )
+        self.authorize_url = f"{base_url}/authorize"
+        self.token_url = f"{base_url}/oauth/token"
+        self.users_url = f"{base_url}/api/v2/users"
+        self.jwt_url = f"{base_url}/.well-known/jwks.json"
+
+    async def get_public_key(self, kid: str) -> str:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(self.jwt_url)
+            jwks = response.json()
+            for key in jwks["keys"]:
+                if key["kid"] == kid:
+                    return key
+        raise InvalidCredentials()
+
+    async def verify_token(self, token: str, headers: dict) -> dict:
+        try:
+            unverified = jwt.decode(token, options={"verify_signature": False})
+            kid = unverified.get("kid")
+
+            public_key_data = await self.get_public_key(kid)
+            public_key = jwt.algorithms.RSAAlgorithm.from_jwk(public_key_data)
+
+            payload = jwt.decode(
+                token,
+                public_key,
+                algorithms=[os.getenv("AUTH0_ALGORITHM")],
+                audience=self.audience,
+                issuer=self.issuer,
+            )
+            return payload
+        except InvalidTokenError as e:
+            raise InvalidCredentials(headers)
+        except ValidationError:
+            raise InvalidCredentials(headers)
+
+    async def get_user(self, user_id: str) -> User | None:
+        """Fetch user from Identity Provider API."""
+        async with httpx.AsyncClient() as client:
+            token_response = await client.post(
+                self.token_url,
+                json={
+                    "client_id": self.client_id,
+                    "client_secret": self.client_secret,
+                    "audience": self.audience,
+                    "grant_type": self.grant_type,
+                },
+            )
+            access_token = token_response.json()["access_token"]
+            user_response = await client.get(
+                f"{self.users_url}/{user_id}",
+                headers={"Authorization": f"Bearer {access_token}"},
+            )
+            auth0_user: dict = user_response.json()
+            return User(
+                id=user_id,
+                name=auth0_user.get("nickname", auth0_user.get("email")),
+                email=auth0_user.get("email"),
+                full_name=auth0_user.get("name"),
+                disabled=auth0_user.get("blocked", False),
+            )
+
+    def get_oauth2_scheme(self, scopes: dict) -> OAuth2:
+        """Create OAuth2 scheme with authorization code flow."""
+        return OAuth2(
+            flows=OAuthFlows(
+                implicit=OAuthFlowImplicit(
+                    authorizationUrl=self.authorize_url, scopes=scopes
+                )
+            )
+        )
+
+
+identity_provider = Auth0provider()
+oauth2_scheme = identity_provider.get_oauth2_scheme(
+    scopes={
+        "openid": "OpenID",
+        "profile": "Profile",
+        "email": "Email",
+        "me": "Read information about the current user.",
+        "items": "Read items.",
+    }
+)
 
 
 class InvalidCredentials(HTTPException):
@@ -84,7 +156,7 @@ class InvalidCredentials(HTTPException):
         super().__init__(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid Authentication credentials",
-            headers={"WWW-Authenticate": "Bearer"},
+            headers=headers,
         )
 
 
@@ -105,18 +177,20 @@ async def get_current_user(
     else:
         authenticate_value = "Bearer"
     headers = {"WWW-Authenticate": authenticate_value}
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username = payload.get("sub")
-        if username is None:
-            raise InvalidCredentials(headers)
-        token_scopes = payload.get("scopes", [])
-        token_data = TokenData(username=username, scopes=token_scopes)
-    except (InvalidTokenError, ValidationError):
+
+    payload = await identity_provider.verify_token(token, headers)
+    username = payload.get("sub")
+    if username is None:
         raise InvalidCredentials(headers)
-    user = get_user(fake_users_db, username=token_data.username)
+
+    # token_scopes = payload.get("scopes", [])
+    token_scopes = payload.get("scope", "").split()
+    token_data = TokenData(username=username, scopes=token_scopes)
+
+    user = await identity_provider.get_user(user_id=token_data.username)
     if user is None:
         raise InvalidCredentials(headers)
+
     for scope in security_scopes.scopes:
         if scope not in token_data.scopes:
             raise InsufficientPermissions(headers)
@@ -133,52 +207,6 @@ async def get_current_active_user(
     return current_user
 
 
-def get_password_hash(password: str):
-    return password_hash.hash(password)
-
-
-def verify_password(plain_password: str | bytes, hashed_password: str | bytes):
-    return password_hash.verify(plain_password, hashed_password)
-
-
-def authenticate_user(
-    fake_db: dict, username: str, password: str
-) -> tuple[bool, UserDBRecord | None]:
-    user = get_user(fake_db, username)
-    if user is None:
-        return False, None
-    if not verify_password(password, user.hashed_password):
-        return False, None
-    return True, user
-
-
-def create_access_token(data: dict, ttl: timedelta | None = None) -> str:
-    data_to_encode = data.copy()
-    if ttl:
-        expire = datetime.now(timezone.utc) + ttl
-    else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
-    data_to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(data_to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-
-@route.post("/token")
-async def login_for_access_token(
-    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
-) -> Token:
-    authenticated, user = authenticate_user(
-        fake_users_db, form_data.username, form_data.password
-    )
-    if not authenticated or user is None:
-        raise InvalidCredentials()
-    access_token_ttl = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.name, "scopes": form_data.scopes}, ttl=access_token_ttl
-    )
-    return Token(access_token=access_token, token_type="bearer")
-
-
 @route.get("/users/me", response_model=User)
 async def read_users_me(
     current_user: Annotated[User, Depends(get_current_active_user)],
@@ -190,4 +218,4 @@ async def read_users_me(
 async def read_own_items(
     current_user: Annotated[User, Depends(get_current_active_user)],
 ):
-    return [{"item_id": "Foo", "owner": current_user.username}]
+    return [{"item_id": "Foo", "owner": current_user.name}]
--- graph_api/main.py
+++ graph_api/main.py
@@ -1,7 +1,32 @@
+import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from graph_api.api.v1.api import route as api_v1_route
 
-app = FastAPI(docs_url="/docs", openapi_url="/docs/openapi.json")
+app = FastAPI(
+    title="Network App API",
+    docs_url="/docs",
+    openapi_url="/docs/openapi.json",
+    swagger_ui_oauth2_redirect_url="/docs/oauth2-redirect",
+    swagger_ui_init_oauth={
+        "clientId": os.getenv("AUTH0_CLIENT_ID"),
+        "scopes": ["openid"],
+        "usePkceWithAuthorizationCodeGrant": True,
+    },
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:8964",
+        "https://localhost:8964",
+        "http://localhost:8080",
+        "https://localhost:8080",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_v1_route, prefix="/v1")
